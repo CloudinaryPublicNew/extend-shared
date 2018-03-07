9a32eca15e7374e5d55f87cfd99806f05229d934
Dan Zeitman <dvpweb@gmail.com>
2018-03-07 11:20:03 -0800

    Updating video-google-transcribe.js

--- video-google-transcribe.js
+++ video-google-transcribe.js
@@ -74,7 +74,7 @@ function transcribeURL(context,cb){
   
   var url = context.data.url || 'http://res.cloudinary.com/de-demo/video/upload/v1520429530/test-audio.mp3' ; 
   
-  var public_id =  parsePath(url).name;
+  var public_id =  parsePath(url).name + "-test";
   
   // explicit 
   cloudinary.v2.uploader.upload(url, 
