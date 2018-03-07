38ce2af6de89f8862fa555a7d42e9ec42805d075
Dan Zeitman <dvpweb@gmail.com>
2018-03-07 08:30:40 -0800

    Update video-google-transcribe.js

--- video-google-transcribe.js
+++ video-google-transcribe.js
@@ -2,6 +2,9 @@
 * @param context {WebtaskContext}
 * https://www.npmjs.com/package/parse-filepath
 * Requires cloudinary and parse-filepath
+* Pass a url  or public_id 
+* https://evangelism.cloudinary.auth0-extend.com/video-google-transcribe?url=<url to resource>>&authKey=<authKey>
+* https://evangelism.cloudinary.auth0-extend.com/video-google-transcribe?public_id=<url to resource>&authKey=<authKey>
 */
 
 
@@ -33,14 +36,11 @@ module.exports = function(context, cb) {
    
 };
 
- 
-//https://res.cloudinary.com/de-demo/video/upload/v1520283833/demo-song.mp3
 
 function transcribe(context,cb){
   
   var public_id = context.data.public_id ||'test-audio';
-  // 'https://res.cloudinary.com/de-demo/video/upload/v1520283833/demo-song.mp3' ; 
-  //'demo-song.mp3';
+ 
   // explicit 
   cloudinary.v2.uploader.explicit(public_id, 
         { 
@@ -68,7 +68,7 @@ function transcribeURL(context,cb){
   
   var url = context.data.url || 'http://res.cloudinary.com/de-demo/video/upload/v1520429530/test-audio.mp3' ; 
   
-  var public_id =  parsePath(url).name + "-test";
+  var public_id =  parsePath(url).name;
   
   // explicit 
   cloudinary.v2.uploader.upload(url, 
