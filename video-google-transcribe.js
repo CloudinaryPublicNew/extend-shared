6a996fca10de0ef8e9d5bec962fd4a22968791f7
Dan Zeitman <dvpweb@gmail.com>
2018-03-07 08:27:11 -0800

    Updating video-google-transcribe.js

--- video-google-transcribe.js
+++ video-google-transcribe.js
@@ -18,8 +18,19 @@ module.exports = function(context, cb) {
       "api_secret": context.secrets.api_secret
     });
     
-    transcribe(context,cb);
-    //transcribeURL(context,cb);
+    // upload
+    
+    if(context.data.url){
+      transcribeURL(context,cb);
+    }
+    
+       
+    // explicit  update
+    if(context.data.public_id){
+      transcribe(context,cb);
+    }
+
+   
 };
 
  
@@ -27,13 +38,13 @@ module.exports = function(context, cb) {
 
 function transcribe(context,cb){
   
-  var public_id = context.data.public_id ||'demo-song';
+  var public_id = context.data.public_id ||'test-audio';
   // 'https://res.cloudinary.com/de-demo/video/upload/v1520283833/demo-song.mp3' ; 
   //'demo-song.mp3';
   // explicit 
   cloudinary.v2.uploader.explicit(public_id, 
         { 
-        public_id: 'demo-song',  
+        public_id: public_id,  
         type: "upload",
         resource_type: "video", 
         raw_convert: "google_speech",
@@ -57,7 +68,7 @@ function transcribeURL(context,cb){
   
   var url = context.data.url || 'http://res.cloudinary.com/de-demo/video/upload/v1520429530/test-audio.mp3' ; 
   
-  var public_id =  parsePath(url).name;
+  var public_id =  parsePath(url).name + "-test";
   
   // explicit 
   cloudinary.v2.uploader.upload(url, 
