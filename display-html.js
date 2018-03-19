514588480e244a35b6b17fa3ccd715228edcc28b
Dan Zeitman <dvpweb@gmail.com>
2018-03-19 14:52:40 -0700

    Updating display-html.js

--- display-html.js
+++ display-html.js
@@ -1,33 +1,14 @@
 /**
 * @param context {WebtaskContext}
 */
-const http = require('http');
 
 
+const request = require('request');
 
 module.exports = function (context, req, res) {
-  
-  console.log(context);
-  
-  var newsletter = context.data.newsletter ||  '2018_03_19_likitacao' ; 
-  
-  var url = 'https://res.cloudinary.com/evino/raw/upload/v1521468541/Newsletter/2018/03_marco/' + newsletter + '/index.html';
-  
-    var options = {
-                    host: 'res.cloudinary.com',
-                    port: 80,
-                    path: url
-                  };
-
-              http.get(options, function(data) {
-                     console.log("Got response: " + data.statusCode);
-                     console.log("Got data: " + JSON.stringify(data,null,5) );
-                     
-                     var htmlData = 'foo';
-                      res.writeHead(200, { 'Content-Type': 'text/html '});
-                      res.end(htmlData);
-              }).on('error', function(e) {
-                console.log("Got error: " + e.message);
-              });
-               
+  var newsletter = context.data.newsletter ||  'Newsletter/2018/03_marco/2018_03_19_likitacao/index.html' ; 
+  var url = 'https://res.cloudinary.com/evino/raw/upload/v1521468541/Newsletter/' + newsletter;
+      res.writeHead(200, { 'Content-Type': 'text/html '});
+       request(url).pipe(res);
 };
+
