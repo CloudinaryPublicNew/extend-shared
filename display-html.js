524aadc37139d3f185dbe0137cfbaacd725ea7c6
Dan Zeitman <dvpweb@gmail.com>
2018-03-19 14:57:31 -0700

    Updating display-html.js

--- display-html.js
+++ display-html.js
@@ -14,7 +14,7 @@
 const request = require('request');
 
 module.exports = function (context, req, res) {
-  var newsletter = context.data.newsletter ||  '2018/03_marco/2018_03_19_likitacao/index.html' ; 
+  var newsletter = context.data.newsletter ||  '2018/03_marco/2018_03_19_likitacao/index.html' ;  // show example if not provided.
   var url = 'https://res.cloudinary.com/evino/raw/upload/v1521468541/Newsletter/' + newsletter;
       res.writeHead(200, { 'Content-Type': 'text/html '});
        request(url).pipe(res);
