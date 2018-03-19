72087d6338984ec71d54c22a252f1349b752dd09
Dan Zeitman <dvpweb@gmail.com>
2018-03-19 15:52:52 -0700

    Updating newsletter.js

--- newsletter.js
+++ newsletter.js
@@ -7,20 +7,20 @@ var app = express();
 
 /**
 * @param context {WebtaskContext}
-*  
+*  Newsletter 
 *  Display HTML resource stored on cloudinary
 * 
 * Example:
-* https://evangelism.cloudinary.auth0-extend.com/display-html?newsletter=2018/03_marco/2018_03_19_likitacao/index.html
+* https://evino.cloudinary.auth0-extend.com/Newsletter/2018/03_marco/2018_03_19_likitacao/index.html
 * 
 * Params:
-* ?newsletter=<path-to-newsletter-resource>
+* .../Newsletter/<path-to-newsletter-resource>
 */
 
 app.use(bodyParser.json());
 
-app.get('/?:resourcepath', function (req, res) {
-    var newsletter =req.resourcepath ||  '2018/03_marco/2018_03_19_likitacao/index.html' ;  // show example if not provided.
+app.get('/?*:resourcepath', function (req, res) {
+  var newsletter = req.resourcepath ||  '2018/03_marco/2018_03_19_likitacao/index.html' ;  // show example if not provided.
   var url = 'https://res.cloudinary.com/evino/raw/upload/v1521468541/Newsletter/' + newsletter;
       res.writeHead(200, { 'Content-Type': 'text/html '});
       request(url).pipe(res);
