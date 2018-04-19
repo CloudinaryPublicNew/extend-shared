83ab6d0d935fdddfcdd9a83ad1c8748150e50128
Dan Zeitman <dvpweb@gmail.com>
2018-04-19 08:02:56 -0700

    Updating cloudyhack-demo.js

--- cloudyhack-demo.js
+++ cloudyhack-demo.js
@@ -4,6 +4,27 @@
 
 var cloudinary = require("cloudinary");
 
+
+function test(context,cb){
+  
+var url = "https://res.cloudinary.com/demo-robert/image/upload/v1523390181/ec18d5b63b46a112b486a97a9d8885d7.jpg";
+var options = {ocr: “adv_ocr”};
+    cloudinary.v2.uploader.upload(url,options, function(error,result) {
+      console.log(result);
+      var ocrResult = result.info.ocr.adv_ocr.data[0].fullTextAnnotation.text || 0,
+      publicId = result.public_id;
+      if (ocrResult !== 0) {
+        cloudinary.v2.uploader.add_tag(ocrResult, publicId,
+          function(result) {
+            console.log(result)
+            cb(null, result);
+        });
+      }
+    });
+    
+}
+
+
 function findByTag(context, cb){
   
   var options ={context:true, tags:true, max_results:500};
