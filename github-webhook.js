f911fdf04c01c940c668138d98c9af2ea6b4d6a2
Dan Zeitman <dvpweb@gmail.com>
2018-08-07 17:25:40 -0700

    Updating github-webhook.js

--- github-webhook.js
+++ github-webhook.js
@@ -5,5 +5,5 @@
 module.exports = function(context, cb) {
   console.log(context.data);
   console.log(context.data.info.ocr.adv_ocr);
-  cb(null, { message:'webhook called see wt logs'});
+  cb(null, { message:'webhook called see wt logs', info:context.data.info});
 };
